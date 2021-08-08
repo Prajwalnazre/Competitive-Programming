d876f5910ccd8292df80da3a1b04525ba2de0279
Prajwalnazre <prajwal.nazre777@gmail.com>
2021-08-08 11:02:01 +0530

    Two Sum problem

---
+++ Two Sum/TwoSum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        li = []
+        for i in range(len(nums)) :
+            for j in range(i + 1, len(nums)) :
+                if (nums[i] + nums[j] == target and i != j) :
+                    li.append(i)
+                    li.append(j)
+                    # print(li)
+        return li
+        
